5c63e330033622eff651a9bb0c0dc7137b5adc29
leeeeee <2480054370@qq.com>
2017-05-27 10:13:42 +0800

    -

--- src/main/java/com/youkke/site/GlobalAdvice.java
+++ src/main/java/com/youkke/site/GlobalAdvice.java
@@ -1,25 +1,17 @@
 package com.youkke.site;
 
 import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
 import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.BindException;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.multipart.MultipartException;
 
 import com.youkke.site.utils.ServiceException;
 
--- src/main/java/com/youkke/site/controller/IndexController.java
+++ src/main/java/com/youkke/site/controller/IndexController.java
@@ -7,11 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.thymeleaf.exceptions.TemplateInputException;
 
-import com.youkke.site.domain.Site;
 import com.youkke.site.domain.Tag;
 import com.youkke.site.service.SiteService;
 
@@ -22,7 +21,7 @@ public class IndexController {
 	private SiteService siteService;
 	
 	@GetMapping(value = "/{provinceId:[a-zA-Z_]{0,10}}")
-	public String test(HttpServletRequest request, Model model){
+	public String test(HttpServletRequest request, Model model) throws Exception {
         String domain =request.getServerName(); 
 		String path = request.getRequestURI().replaceAll("/", "");
 		List<Tag> tags = siteService.findCurrentTags(domain, path);
